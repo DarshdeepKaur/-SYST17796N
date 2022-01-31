580e66668e0373a87974d80d56e505b3e06f99e6
Darshdeep Kaur <kadarshd@sheridancollege.ca>
2022-01-31 13:30:22 -0500

    First Commit

---
+++ src/shape/Square.java
@@ -0,0 +1,25 @@
+
+
+package shape;
+
+/**
+ *This class +++ Insert class description here +++
+ * @author Darshdeep Kaur 
+ */
+public class Square {
+private int length ;
+
+    /**
+     * @return the length
+     */
+    public int getLength() {
+        return length;
+    }
+
+    /**
+     * @param length the length to set
+     */
+    public void setLength(int length) {
+        this.length = length;
+    }
+}
