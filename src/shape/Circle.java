87eeaae0b7dd86938944704b66d2848636fdc9a6
Darshdeep Kaur <kadarshd@sheridancollege.ca>
2022-01-31 10:03:21 -0500

    First Commit

---
+++ src/shape/Circle.java
@@ -0,0 +1,25 @@
+
+
+package shape;
+
+/**
+ *This class +++ Insert class description here +++
+ * @author Darshdeep Kaur 
+ */
+public class Circle {
+private int radius;
+
+    /**
+     * @return the radius
+     */
+    public int getRadius() {
+        return radius;
+    }
+
+    /**
+     * @param radius the radius to set
+     */
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+}
---
+++ src/shape/Shape.java
@@ -0,0 +1,19 @@
+
+
+package shape;
+
+/**
+ *This class +++ Insert class description here +++
+ *
+ * @author Darshdeep Kaur  
+ */
+public class Shape {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+    }
+
+}
