05c6f71eaaf88b110cf2814b1053cb30dccdcba6
Darshdeep Kaur <kadarshd@sheridancollege.ca>
2022-01-31 13:45:33 -0500

    First Commit

---
+++ src/shape/Symmetricshape.java
@@ -0,0 +1,26 @@
+
+
+package shape;
+
+/**
+ *This class +++ Insert class description here +++
+ * @author Darshdeep Kaur 
+ */
+public class Symmetricshape {
+    // divide shape winto two equal parts 
+ private int divide;
+
+    /**
+     * @return the divide
+     */
+    public int getDivide() {
+        return divide;
+    }
+
+    /**
+     * @param divide the divide to set
+     */
+    public void setDivide(int divide) {
+        this.divide = divide;
+    }
+}
